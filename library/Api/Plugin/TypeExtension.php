1dd25998084157824bd46fa8e173f0421b27585f
rroper <rroper@adknowledge.com>
2012-06-01 17:59:49 -0500

    Fixed extensions when no querystring is provided

--- library/Api/Plugin/TypeExtension.php
+++ library/Api/Plugin/TypeExtension.php
@@ -8,8 +8,7 @@ class Api_Plugin_TypeExtension extends Zend_Controller_Plugin_Abstract
     public function routeStartup(Zend_Controller_Request_Abstract $request)
     {
 
-        $url = preg_replace('/\.[a-z]+$/','',$_SERVER['REQUEST_URI']);
-        $url = preg_replace('/\.[a-z]+(\?.*)$/','$1',$url);
+        $url = preg_replace('/\.[a-z]+(\?.*)?$/','$1',$_SERVER['REQUEST_URI']);
 		$request->setRequestUri($url);
 
     }
