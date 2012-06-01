fa90c06f4176d70d142bedc0d5774974421b7611
rroper <rroper@adknowledge.com>
2012-06-01 15:38:40 -0500

    Added .table support, and removed AclCheck logic

--- library/Api/Plugin/AclCheck.php
+++ library/Api/Plugin/AclCheck.php
@@ -12,20 +12,6 @@ class Api_Plugin_AclCheck extends Zend_Controller_Plugin_Abstract
     
 	public function preDispatch(Zend_Controller_Request_Abstract $request)
 	{
-	    $acl = Zend_Registry::get('acl');
-        $role = 'guest';
-        if (Zend_Registry::isRegistered('profile')) {
-            $profile = Zend_Registry::get('profile');
-            $role = $profile['role'];
-
-        }
-
-		$target = '/' . $request->getControllerName() . '/' . $request->getActionName();
-
-		if($acl->has($target) && (!$acl->isAllowed($role,$target))) {
-			$request->setControllerName('error');
-			$request->setActionName('access');
-		}
 	}
     
 }
